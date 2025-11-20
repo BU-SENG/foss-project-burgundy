91fe936cd8d9a83ef17e3ce3ff320b1bfb77d126
Runoxdd <runoefekemo@gmail.com>
2025-11-20 08:50:55 +0100

    changed something in backend

--- backend/app.js
+++ backend/app.js
@@ -20,14 +20,14 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: ['http://localhost:5173']},
+  cors: { origin: ['*']},
 });
 
 // Connect to DB
 connectToMongo();
 
 // Middleware
-app.use(cors({ origin: ['http://localhost:5173'], methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'] }));
+app.use(cors({ origin: ['*'], methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
